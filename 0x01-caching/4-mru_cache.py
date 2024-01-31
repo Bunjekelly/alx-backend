fb8760b4fa6a5ccaae7c7ebc30c184d5fbebcaf5
Bunje Kelly <kellybunje@gmail.com>
2024-01-31 10:31:32 +0000

    mru cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/biin/env python3
+
+"""a class MRUCache that inherits from BaseCaching and is a caching system"""
+
+from base_caching import BaseCaching
+from collections import deque
+
+
+class MRUCache(BaseCaching):
+    """a class MRUCache that inherits from BaseCaching and is
+    a caching system with put and get methods"""
+
+    def __init__(self):
+        """method for initialization"""
+        super().__init__()
+        self.keys = deque()
+
+    def put(self, key, item):
+        """assigns to the dictionary self.cache_data
+        the item value for the key key"""
+        if key is None or item is None:
+            return
+        if key in self.keys:
+            self.keys.remove(key)
+        elif len(self.keys) >= BaseCaching.MAX_ITEMS:
+            discarded_key = self.keys.pop()
+            del self.cache_data[discarded_key]
+            print('DISCARD: {}'.format(discarded_key))
+        self.cache_data[key] = item
+        self.keys.append(key)
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+        if key is None or key not in self.cache_data:
+            return None
+        self.keys.remove(key)
+        self.keys.append(key)
+        return self.cache_data[key]
