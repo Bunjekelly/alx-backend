87ada0828d6d525d039752707e523b6fd6cb5988
Bunje Kelly <kellybunje@gmail.com>
2024-01-31 09:45:40 +0000

    executable

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,11 +10,13 @@ class BasicCache(BaseCaching):
     and is a caching system with two methods put and get"""
 
     def put(self, key, item):
+        """assigns to the dictionary self.cache_data the item value for the key key"""
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """"returns the value in self.cache_data linked to key"""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
